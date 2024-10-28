ad5742abeec4e6f78fa59fba1976ecdacc5246a7
shaik881 <shaiksaquib8182@gmail.com>
2024-10-28 18:52:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.WebSockets;
+using System.Text;
+var ws = new ClientWebSocket();
+string name;
+while (true)
+{
+    Console.WriteLine("Input name: ");
+    name = Console.ReadLine();
+    break;
+}
+Console.WriteLine("Connecting to server");
+await ws.ConnectAsync(new Uri($"wss://localhost:6969/ws?name={name}"), CancellationToken.None);
+Console.WriteLine("Connected");
+var sendtask = Task.Run(async () =>
+{
+    while (true)
+    {
+        var message = Console.ReadLine();
+        if (message == "exit")
+        {
+            break;
+        }
+        var bytes = Encoding.UTF8.GetBytes(message);
+        await ws.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+});
+var receiveTask = Task.Run(async () =>
+{
+    var buffer = new byte[1024];
+    while (true)
+    {
+        var result = await ws.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        if (result.MessageType == WebSocketMessageType.Close)
+        {
+            break;
+        }
+        var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+        Console.WriteLine(message);
+    }
+});
+await Task.WhenAll(sendtask, receiveTask);
+if (ws.State == WebSocketState.Closed)
+{
+    await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+}
+await Task.WhenAll(sendtask, receiveTask);
